1e1a2c1eb78a451cfab2075a32da71a0ea630c63
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-21 11:25:28 +0530

    adding dynamic encoded address

---
+++ playground/async.js
@@ -0,0 +1,28 @@
+// console.log('Starting app...');
+
+// setTimeout(() => {
+//     console.log("Inside a callback");
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("Second call back");
+// }, 0);
+
+// console.log('Finishing up...');
+
+// var getUser = (id, callback) => {
+//     var user = {
+//         id, 
+//         name: 'Hari'
+//     };
+    
+//     setTimeout(() => {
+//         callback(user);
+//     }, 3000);
+// };
+
+// getUser(31, (userObj) => {
+//     console.log(userObj);
+// })
+
+
