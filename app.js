e4b5035b8f378e68294a68e7b88184767fb79492
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-21 12:42:18 +0530

    refactoring and adding callbacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,19 +15,11 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&sensor=true&key=AIzaSyA6vvoQi4GrJ3kyoABBITbrj9x7X8uHAKw`,
-    json: true
-}, (error, response, body) => {
-    if(error){
-        console.log('Unable to connect to google servers.');
-    } else if(body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find your address');
-    } else if(body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    } else {
+        // console.log(results);
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
